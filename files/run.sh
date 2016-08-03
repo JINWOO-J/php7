866cdad10835bc1e341e3b138c082742603eb6e1
JINWOO-J <jinwoo@yellotravel.com>
2016-08-03 15:50:10 +0900

    first commit

---
+++ files/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export FPM_LISTEN=${FPM_LISTEN:-"0.0.0.0:9000"}
+export FPM_USER=${FPM_USER:-"www-data"}
+export FPM_GROUP=${FPM_GROUP:-"www-data"}
+export USE_DOCKERIZE=${USE_DOCKERIZE:-"yes"}
+export PHP_INI_DIR=${PHP_INI_DIR:-"/etc/php"}
+export EXTRACONF=${EXTRACONF:-";"}
+
+if [ $USE_DOCKERIZE == "yes" ];
+then
+    echo "USE the dockerize template";
+    dockerize -template /etc/php/php-fpm.tmpl > /etc/php/php-fpm.conf
+fi
+
+/usr/local/sbin/php-fpm-env >> /etc/php/php-fpm.conf
+/usr/local/sbin/php-fpm -c ${PHP_INI_DIR} --nodaemonize
